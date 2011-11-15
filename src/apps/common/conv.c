79d9be513df17fa77edaaedecf9b12e4ad7b3be7
ecicek <ecicek@8357e56b-4c1e-0410-8926-d51ee95e2dac>
2011-11-15 12:58:59 +0000

    git-svn-id: https://svn.pls.mpi-sws.org/PLS/svn-reps/project/ceal@19566 8357e56b-4c1e-0410-8926-d51ee95e2dac

---
+++ src/apps/common/conv.c
@@ -0,0 +1,68 @@
+/* Ezgi Cicek <ecicek@mpi-sws.org>
+ * Convolution with 1-1 correspondence
+ * 1 pixel for each 1 modref
+ */
+#ifndef __CONV_C__
+#define __CONV_C__
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+long ROW = 256;
+long COL = 256;
+long K =2;
+typedef long pixel_t;
+typedef double kernel_t;
+typedef unsigned char raw_t;
+
+///////////////////////////////////////////////////////////////////////////////
+// load 8-bit RAW image
+///////////////////////////////////////////////////////////////////////////////
+void loadRawImage(char *fileName, int x, int y, pixel_t *data)
+{
+    // check params
+    if(!fileName || !data)
+        printf(" either file does not exist or data is empty");
+
+    FILE *fp;
+    if((fp = fopen(fileName, "r")) == NULL)
+    {
+        printf("Cannot open %s.\n", fileName);
+    }
+
+    // read pixel data
+    int i, j;
+    for ( i = 0; i < x*y; i++){
+        fread(data+i, sizeof(raw_t), 1, fp);
+    }
+    //fread(data, sizeof(pixel_t), x*y, fp);
+    fclose(fp);
+
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// write 8-bit RAW image
+///////////////////////////////////////////////////////////////////////////////
+void writeRawImage(char *fileName, int x, int y, pixel_t *data)
+{
+    // check params
+    if(!fileName || !data)
+        printf(" either file does not exist or data is empty");
+
+    FILE *fp;
+    if((fp = fopen(fileName, "wa+")) == NULL)
+    {
+        printf("Cannot open %s.\n", fileName);
+    }
+
+    int i, j;
+    for ( i = 0; i < x*y; i++){
+        fwrite(data+i, sizeof(raw_t), 1, fp);
+    }
+    // write pixel data
+    //fwrite(data, sizeof(pixel_t), x*y, fp);
+    fclose(fp);
+
+}
+#endif
