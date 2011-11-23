c461d9d608961b8911118992ec58edbfcc486d21
hammer <hammer@8357e56b-4c1e-0410-8926-d51ee95e2dac>
2011-11-23 08:51:23 +0000

    fixed a subtle stability bug; mustafa noticed it during class
    git-svn-id: https://svn.pls.mpi-sws.org/PLS/svn-reps/project/ceal@19759 8357e56b-4c1e-0410-8926-d51ee95e2dac

--- src/apps/example_small/example_small.c
+++ src/apps/example_small/example_small.c
@@ -1,61 +1,105 @@
+/* Matthew Hammer <hammer@mpi-sws.org> */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "cealtesthook_dummies.c"
 
+#define print(FMT,...) printf("%s:%d: " FMT "\n", __FUNCTION__,__LINE__,##__VA_ARGS__)
 
+// #define BASIC_VERSION
+#define SEPARATE_FUN_VERSION
+// #define CUT_BLOCK_VERSION
+// #define CUT_EXPR_VERSION
+// #define MEMO_STMT_VERSION
+
+
+#ifdef BASIC_VERSION
 /* This version is the most straightforward. */
-void max_of_squares_1(long* in1, long* in2, long* out) {
+/* But, if we pretend that squaring a long is very costly, then it is
+   not stable: sometimes it will re-evaluate this operation
+   needlessly. */
+void max_of_squares(long* in1, long* in2, long* out) {
   long max;
   
   if(labs(*in1) > labs(*in2)) {
-    max = *in1;
+    max = labs(*in1);
   }
   else {
-    max = *in2;
+    max = labs(*in2);
   }
 
-  printf(__FUNCTION__  ": max is %ld, computing its square..\n", max);
+  print("max is %ld, computing its square..", max);
   *out = (max * max);
 }
 
-/* This version is like max_squares_1, except that it avoids unneeded
-   re-multiplication via a memoization point. */
-void max_of_squares_2(long* in1, long* in2, long* out) {
+#elif defined SEPARATE_FUN_VERSION
+long max_of_labs(long* in1, long* in2) {
+  return labs(*in1) > labs(*in2)
+    ? labs(*in1)
+    : labs(*in2) ;
+}
+
+void max_of_squares(long* in1, long* in2, long* out) {
+  long max = max_of_labs(in1, in2);
+  print("max is %ld, computing its square..", max);
+  *out = (max * max);
+}
+
+#elif defined CUT_EXPR_VERSION
+void max_of_squares(long* in1, long* in2, long* out) {
   long max;
-  
-  if(labs(*in1) > labs(*in2)) {
-    max = *in1;
+
+  /* The cast to long is needed by CEAL for obscure reasons.
+     (See also: the 'minimum-modref-size' restriction). */
+  if(cut( (long) (labs(*in1) > labs(*in2)) )) {
+    max = labs(*in1);
   }
   else {
-    max = *in2;
+    max = labs(*in2);
   }
 
-  memo;
-  printf(__FUNCTION__ ": max is %ld, computing its square..\n", max);
+  print("max is %ld, computing its square..", max);
   *out = (max * max);
 }
 
-/* This version is like max_squares_1, except that it avoids unneeded
-   re-multiplication via a cut block. */
-void max_of_squares_3(long* in1, long* in2, long* out) {
+#elif defined CUT_BLOCK_VERSION
+void max_of_squares(long* in1, long* in2, long* out) {
   long max;
 
   cut {
     if(labs(*in1) > labs(*in2)) {
-      max = *in1;
+      max = labs(*in1);
     }
     else {
-      max = *in2;
+      max = labs(*in2);
     }
   }
 
-  printf(__FUNCTION__ ": max is %ld, computing its square..\n", max);
+  print("max is %ld, computing its square..", max);
   *out = (max * max);
 }
 
+#elif defined MEMO_VERSION 
+void max_of_squares(long* in1, long* in2, long* out) {
+  long max;
+  
+  if(labs(*in1) > labs(*in2)) {
+    max = labs(*in1);
+  }
+  else {
+    max = labs(*in2);
+  }
+
+  memo;
+  print("max is %ld, computing its square..", max);
+  *out = (max * max);
+}
+
+#endif
+
 void print_inout(long* in1, long* in2, long* out) {
-  printf(__FUNCTION__ ": input values   : %ld and %ld\n", *in1, *in2);
-  printf(__FUNCTION__ ": max of squares : %ld\n", *out);
+  print("input values are %ld and %ld", *in1, *in2);
+  print("max of squares is %ld", *out);
 }
 
 
@@ -69,13 +113,15 @@ int main(int argc, char** argv) {
   long z;
 
   /* Invoke the core. */
-  core(max_of_squares_1)(&x, &y, &z);
+  print("running core..");
+  core(max_of_squares)(&x, &y, &z);
   print_inout(&x, &y, &z);
   
   /* Mutate the input. */     
   x = -4;
 
   /* Invoke change propagation. */
+  print("running change propagation..");
   propagate;
   print_inout(&x, &y, &z);
 
@@ -83,6 +129,7 @@ int main(int argc, char** argv) {
   y = 3;
 
   /* Invoke change propagation. */
+  print("running change propagation..");
   propagate;
   print_inout(&x, &y, &z);
 
