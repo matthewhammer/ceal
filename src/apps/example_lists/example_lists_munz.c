d8b4a986e666868b53240b2883fb76b7ae1a61ec
munz <munz@8357e56b-4c1e-0410-8926-d51ee95e2dac>
2011-12-12 02:54:07 +0000

    git-svn-id: https://svn.pls.mpi-sws.org/PLS/svn-reps/project/ceal@20137 8357e56b-4c1e-0410-8926-d51ee95e2dac

---
+++ src/apps/example_lists/example_lists_munz.c
@@ -0,0 +1,121 @@
+/* Matthew Hammer <hammer@mpi-sws.org> */
+
+/* Type definitions. */
+typedef struct cons_s cons_t;
+typedef cons_t* list_t;
+struct cons_s {
+  data_t hd;
+  list_t tl;
+} list_t;
+
+/* List construction. */
+
+/* -- nil constructor. */
+#define nil ((list_t) NULL)
+
+/* -- cons constructor.
+   (Tail field to be filled in later.) */
+list_t list_cons(data_t d) {
+  list_t cons = alloc( cons_t );
+  cons->hd = d;
+  return cons;
+}
+
+/* -- cons constructor.
+   (Tail field filled in during construction.) */
+list_t list_cons_(data_t hd, list_t tl) {
+  cons_t* c = list_cons(hd);
+  c->tl = tl;
+  return c;
+}
+
+void list_print_rec(list_t in) {
+  if( in ) {
+    printf("%ld", in->hd);
+    if( in->tl ) {
+      printf(", ");
+      list_print_rec(in->tl);
+    }
+  }
+}
+
+void list_print(const char* name, list_t in) {
+  printf("%s: [", name);
+  list_print_rec( in );
+  printf("]\n");
+}
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - */
+/* TODO:
+   1. Copy this file to example_lists_<username>.c
+   2. svn add example_lists_<username>.c
+   3. In your copy, insert cuts/memos below as needed.
+*/
+
+void list_map(list_t in, list_t* out) { db("");
+  if(in == nil) {
+    *out = nil;
+  }
+  else {
+    data_t hd = in->hd;
+    list_t new_cons;
+    memo { new_cons = list_cons( some_function( hd ) ); }
+    *out = new_cons;
+    memo;
+    list_map ( in->tl, & new_cons->tl );
+  }
+}
+
+void list_filter(list_t in, list_t* out) { db("");
+  if(in == nil) {
+    *out = nil;
+  }
+  else {
+    if( some_predicate( in->hd ) ) {
+      list_t new_cons;
+      memo { new_cons = list_cons( in->hd ); }
+      *out = new_cons;
+      memo;
+      list_filter ( in->tl, & new_cons->tl );
+    }
+    else {
+      memo;
+      list_filter ( in->tl, out );
+    }
+  }
+}
+
+void list_split(list_t in, list_t* out1, list_t* out2) { db("");
+  if(in == nil) {
+    *out1 = nil;
+    *out2 = nil;
+  }
+  else {
+    list_t new_cons;
+    memo { new_cons = list_cons( in->hd ); }
+
+    if( some_predicate( in->hd ) ) {
+      *out1 = new_cons;
+      memo;
+      list_split ( in->tl, & new_cons->tl, out2 );
+    }
+    else {
+      *out2 = new_cons;
+      memo;
+      list_split ( in->tl, out1, & new_cons->tl );
+    }
+  }
+}
+
+void list_reverse(list_t in, list_t rev, list_t* out) { db("");
+  if( in == nil ) {
+    *out = rev;
+  }
+  else {
+    list_t new_cons;
+    memo { new_cons = list_cons( in->hd ); }
+    new_cons->tl = rev;
+    memo;
+    list_reverse (in->tl, new_cons, out);
+  }
+}
